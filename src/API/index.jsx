b116fd60b9b583e661dbfd1796e018201f3c97b4
emircandemr <72731296+emircandemr@users.noreply.github.com>
2022-07-07 22:11:16 +0300

    update

--- src/API/index.jsx
+++ src/API/index.jsx
@@ -1,5 +1,5 @@
 
 export const REACT_APP_API_KEY = "20280d9d9065308790d5429a21ac9d10";
-export const REACT_APP_URL_LAT = "http://api.openweathermap.org/geo/1.0/direct?q=";
-export const REACT_APP_URL = "http://api.openweathermap.org/data/2.5/onecall?";
+export const REACT_APP_URL_LAT = "https://api.openweathermap.org/geo/1.0/direct?q=";
+export const REACT_APP_URL = "https://api.openweathermap.org/data/2.5/onecall?";
 export const REACT_APP_ICON_URL = "https://openweathermap.org/img/wn/"
